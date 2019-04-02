87568e190e4419e539c13bdf1933cfd47624e58f
wilsonbraga <wilsonrodriguesbraga@gmail.com>
2019-04-02 17:19:35 -0300

    correção da senha local

--- src/main/java/com/algaworks/brewer/config/MailConfig.java
+++ src/main/java/com/algaworks/brewer/config/MailConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -15,9 +14,9 @@ import com.algaworks.brewer.mail.Mailer;
 
 @Configuration
 @ComponentScan(basePackageClasses = Mailer.class)
-@PropertySource( value = { "classpath:env/mail-${ambiente:local}.properties" })
-@PropertySource( value = { "file://${HOME}/.brewer-mail.properties"}, ignoreResourceNotFound = true)
-//@PropertySource({ "classpath:env/mail-homolog.properties"})
+//@PropertySource( value = { "classpath:env/mail-${ambiente:local}.properties" })
+//@PropertySource( value = { "file://${HOME}/.brewer-mail.properties"}, ignoreResourceNotFound = true)
+//@PropertySource({ "classpath:env/mail-prod.properties"})
 public class MailConfig {
 	
 	@Autowired
